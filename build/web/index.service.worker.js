d231a7e9d58919c28cc90abc4c0625b1fdf1509d
x8128 <aliasbonfire@gmail.com>
2026-05-07 07:30:07 +0900

    클로드가 웹빌드

--- build/web/index.service.worker.js
+++ build/web/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1778106158|4806026107';
+const CACHE_VERSION = '1778106517|16573954';
 /** @type {string} */
 const CACHE_PREFIX = 'TamaWorld-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
