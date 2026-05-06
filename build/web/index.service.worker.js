8cfc8755610f9137ccc4c1d04e54395d30040e17
x8128 <aliasbonfire@gmail.com>
2026-05-07 07:16:04 +0900

    빌드파일 수정

--- build/web/index.service.worker.js
+++ build/web/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1778105031|3678960978';
+const CACHE_VERSION = '1778105755|4403272490';
 /** @type {string} */
 const CACHE_PREFIX = 'TamaWorld-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
